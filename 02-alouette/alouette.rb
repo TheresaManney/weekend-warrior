c6812dec1e5b9125f646bc3e6a5f0b793b1a7f12
Theresa Manney <theresa.m.17@gmail.com>
2017-02-25 14:32:04 -0800

    Created outline for Alouette

---
+++ 02-alouette/alouette.rb
@@ -0,0 +1,17 @@
+class Alouette
+
+  def initialize()
+  end
+
+  def lines_for_verse
+    # Returns an array of strings without repeats
+  end
+
+  def verse
+    # will build the requested verse...it should return a string
+  end
+
+  def sing
+    # will build the entire song
+  end
+end
