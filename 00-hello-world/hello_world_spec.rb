43ed367509e68867a0b763014485c53dcef597d9
Theresa Manney <theresa.m.17@gmail.com>
2017-02-27 09:47:21 -0800

    Hello world complete

--- 00-hello-world/hello_world_spec.rb
+++ 00-hello-world/hello_world_spec.rb
@@ -3,7 +3,10 @@ require 'minitest/pride'
 require 'minitest/autorun'
 require_relative 'hello_world'
 
+# when we create do, we create a block!
 describe "Hello World" do
+  #each it is consitered one test case
+  #'it' should be thought of as a title
   it "When given no name, it should greet the world!" do
     expect(hello_world).must_equal 'Hello, World!'
   end
@@ -20,3 +23,6 @@ describe "Hello World" do
     expect(hello_world '').must_equal 'Hello, World!'
   end
 end
+
+#skips were used to skip certain blocks of code so other blocks of code can be tested with this hello_world_spec.rb
+#skips were added one line before expect...
